3abc499005cd36d9ac336e549f06b21a1095dba7
Rodolfo Vieira <rodolfovieira95@outlook.com>
2023-02-20 02:00:14 -0300

    feat: setup styled components on storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../src/theme";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +11,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+const themes = [theme];
+addDecorator(withThemesProvider(themes), ThemeProvider);
