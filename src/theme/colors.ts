4a34e2a8799949d535f74c1130cac69ea06c3808
Rodolfo Vieira <rodolfovieira95@outlook.com>
2023-02-20 02:01:00 -0300

    feat: setup a styled-components theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  text: "#000",
+  background: "#fff",
+  primary: "#0085C3", // Must be HEX
+  secondary: "#00a",
+  gray: "#eee",
+  lightgray: "#fafafa",
+  modes: {
+    light: {
+      background: "#fff",
+      text: "#000",
+      primary: "#0cf",
+      secondary: "#f0e",
+      gray: "#222",
+      lightgray: "#111",
+    },
+    dark: {
+      text: "#fff",
+      background: "#000",
+      primary: "#0cf",
+      secondary: "#f0e",
+      gray: "#222",
+      lightgray: "#111",
+    },
+    // other color modes...
+  },
+};
---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+import { breakpoints } from "./breakpoints";
+import { colors } from "./colors";
+import { font } from "./fonts";
+import { spacing } from "./spacing";
+import { transitions } from "./transitions";
+import { zindeces as zindex } from "./zindex";
+
+export const theme = {
+  breakpoints,
+  colors,
+  font,
+  spacing,
+  transitions,
+  zindex,
+};
